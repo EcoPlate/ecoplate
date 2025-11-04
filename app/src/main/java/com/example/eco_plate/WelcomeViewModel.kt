23396a5d0f77362b29b7a12b80e04d667a49c25f
dtcoops <danielcooper179@gmail.com>
2025-11-04 12:43:26 -0800

    Signup fragment setup

--- app/src/main/java/com/example/eco_plate/WelcomeViewModel.kt
+++ app/src/main/java/com/example/eco_plate/WelcomeViewModel.kt
@@ -12,9 +12,12 @@ class WelcomeViewModel(application: Application) : AndroidViewModel(application)
     private val preferences = application.getSharedPreferences("login_prefs", Context.MODE_PRIVATE)
     private var _loginEmail = MutableLiveData<String>("")
     private var _loginPassword = MutableLiveData<String>("")
+    private var _confirmPassword = MutableLiveData<String>("")
 
     val loginEmail: LiveData<String> get() = _loginEmail
     val loginPassword: LiveData<String> get() = _loginPassword
+    val confirmPassword: LiveData<String> get() = _confirmPassword
+    val accountType = MutableLiveData<String>()
     val isRemembered: Boolean get() = preferences.getBoolean("remember_me", false)
     val hasSavedLogin: Boolean get() = isRemembered && !getSavedEmail().isNullOrBlank()
 
@@ -40,5 +43,9 @@ class WelcomeViewModel(application: Application) : AndroidViewModel(application)
 
     fun setLoginPassword(value: String) {
         _loginPassword.value = value
+    }
+
+    fun setAccountType(value: String) {
+        accountType.value = value
     }
 }
--- app/src/main/java/com/example/eco_plate/ui/SignupFragment.kt
+++ app/src/main/java/com/example/eco_plate/ui/SignupFragment.kt
@@ -1,19 +1,37 @@
 package com.example.eco_plate.ui
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.example.eco_plate.WelcomeActivity
 import com.example.eco_plate.WelcomeViewModel
 import com.example.eco_plate.databinding.FragmentSignupBinding
 
+// TODO
+// Need to implement password match
+// need to pass new account to backend
+// need to ensure button doesn't succeed without email and matching passwords
 class SignupFragment : Fragment() {
 
     private var _binding: FragmentSignupBinding? = null
     private val binding get() = _binding!!
     private val viewModel: WelcomeViewModel by activityViewModels()
+
+    private lateinit var emailET : EditText
+    private lateinit var passwordET : EditText
+    private lateinit var confirmPasswordET: EditText
+    private lateinit var accountTypeSpinner: Spinner
+    private lateinit var createAccountBTN : Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,5 +47,72 @@ class SignupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        emailET = binding.signupEmail
+        passwordET = binding.signupPassword1
+        confirmPasswordET = binding.signupPassword2
+        accountTypeSpinner = binding.signupSpinner
+        createAccountBTN = binding.btnSignup
+
+        // Email
+        viewModel.loginEmail.observe(viewLifecycleOwner) { loginEmail ->
+            val current = emailET.text?.toString().orEmpty()
+            if (current != loginEmail) emailET.setText(loginEmail)
+        }
+        emailET.doAfterTextChanged { text ->
+            val emailString = text?.toString().orEmpty()
+            if (viewModel.loginEmail.value != emailString) viewModel.setLoginEmail(emailString)
+        }
+
+        viewModel.loginPassword.observe(viewLifecycleOwner) { loginPassword ->
+            passwordET.setText(loginPassword)
+        }
+        passwordET.doAfterTextChanged { text ->
+            val passwordString = text?.toString().orEmpty()
+            if (viewModel.loginPassword.value != passwordString) viewModel.setLoginPassword(passwordString)
+        }
+
+        viewModel.confirmPassword.observe(viewLifecycleOwner) { confirmPassword ->
+            passwordET.setText(confirmPassword)
+        }
+        passwordET.doAfterTextChanged { text ->
+            val passwordString = text?.toString().orEmpty()
+            if (viewModel.loginPassword.value != passwordString) viewModel.setLoginPassword(passwordString)
+        }
+
+        val accountTypes = listOf("Buyer", "Seller")
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.simple_spinner_dropdown_item,
+            accountTypes
+        )
+        accountTypeSpinner.adapter = adapter
+        viewModel.accountType.observe(viewLifecycleOwner) { accountType ->
+            val position = accountTypes.indexOf(accountType)
+            if (position >= 0 && accountTypeSpinner.selectedItemPosition != position) {
+                accountTypeSpinner.setSelection(position)
+            }
+        }
+
+        accountTypeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                p0: AdapterView<*>?,
+                p1: View?,
+                p2: Int,
+                p3: Long
+            ) {
+                val selectedType = accountTypes[p2]
+                if (viewModel.accountType.value != selectedType) {
+                    viewModel.setAccountType(selectedType)
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+        }
+
+        createAccountBTN.setOnClickListener {
+            (requireActivity() as WelcomeActivity).goToMain()
+        }
     }
 }
